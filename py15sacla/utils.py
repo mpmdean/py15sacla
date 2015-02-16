9e8f9e804089c2ee156ba716025305975340ea66
Pavol Juhas <pavol.juhas@gmail.com>
2015-02-16 13:55:59 -0500

    Add histogram function optimized for equal bins.

---
+++ py15sacla/utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'''Shared utility functions.
+'''
+
+
+def eqbinhistogram(a, bins=10, range=None):
+    '''Calculate histogram over uniformly spaced bins.
+    This uses numpy.bincount which is significantly faster than histogram.
+
+    a    -- input array of arbitrary dimensions.  Histogram is computed
+            over flattened array.
+    bins -- number of equal-width bins, by default 10.
+    range -- an optional tuple of (lobound, hibound) range for the bins.
+            Values outside the range are ignored.  When not provided,
+            use (a.min(), a.max()).
+
+    Return a tuple of (counts, bin_edges).
+    '''
+    import numpy
+    a = numpy.asarray(a)
+    if range is not None:
+        lo, hi = range
+        a1 = a[numpy.logical_and(lo <= a, a <= hi)]
+    else:
+        lo, hi = a.min(), a.max()
+        a1 = a.reshape(-1)
+    a2 = (a1 - lo) * (bins / (hi - lo))
+    counts = numpy.bincount(a2.astype(int), minlength=bins)
+    if len(counts) > bins:
+        counts[-2:-1] += counts[-1:]
+        counts = counts[:-1]
+    assert len(counts) == bins
+    bin_edges = numpy.linspace(lo, hi, bins + 1)
+    return (counts, bin_edges)
