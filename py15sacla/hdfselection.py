e83327bdcdc6880207c4b0e170310e3884324d59
Pavol Juhas <pavol.juhas@gmail.com>
2015-02-19 16:01:04 -0500

    Use read-only file mode by default.
    Add optional mode argument to HDFSelection.
    Improve argument checking.

--- py15sacla/hdfselection.py
+++ py15sacla/hdfselection.py
@@ -9,32 +9,37 @@ from py15sacla.multipattern import MultiPattern
 class HDFSelection(object):
 
     hdffile = None
-    datanames = None
+    _datanames = None
 
-    def __init__(self, src, pattern=''):
+    def __init__(self, src, pattern='', mode=None):
         '''Initialize new HDFSelection
 
         src  -- source node for selecting HDF5 datasets.  Accepted types
                 are HDFSelection, HDF5 Group or a string, which is used
                 to open an h5py.File.
-        pattern  -- optional string pattern for filtering datanames.
+        pattern  -- optional string pattern for filtering _datanames.
                 By default an empty string that matches everything.
+        mode -- Python mode used for opening the HDF5 file, by default 'r'.
+                Used only when src is a string.
         '''
-        self.datanames = []
+        if mode is not None and not isinstance(src, basestring):
+            raise ValueError("mode is valid only when src is a filename.")
+        self._datanames = []
         collect_datanames = lambda n, v: (isinstance(v, h5py.Dataset) and
-                self.datanames.append(n) or None)
+                self._datanames.append(n) or None)
         if isinstance(src, HDFSelection):
             self.hdffile = src.hdffile
-            self.datanames[:] = src.datanames
+            self._datanames[:] = src._datanames
         elif isinstance(src, basestring):
-            self.hdffile = h5py.File(src)
+            m = 'r' if mode is None else mode
+            self.hdffile = h5py.File(src, mode=m)
             self.hdffile.visititems(collect_datanames)
         elif isinstance(src, h5py.Group):
             self.hdffile = src.file
             src.visititems(collect_datanames)
         else:
-            raise ValueError("Unsupported selection source {0!r}".format(src))
+            raise TypeError("Unsupported selection source {0!r}.".format(src))
         if pattern:
             mp = MultiPattern(pattern)
-            self.datanames = [n for n in self.datanames if mp.match(n)]
+            self._datanames = [n for n in self._datanames if mp.match(n)]
         return
