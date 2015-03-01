2214bd5d23462d303255a8d6f32d553dea65a1b9
Pavol Juhas <pavol.juhas@gmail.com>
2015-02-28 19:12:43 -0500

    Module for convenient imports to IPython session.

---
+++ py15sacla/ipy_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''Import functions and classes useful for interactive session.
+This can be used from IPython as
+
+from py15sacla.ipy_all import *
+'''
+
+from py15sacla.ccdframes import CCDFrames
+from py15sacla.hdfselection import HDFSelection
+from py15sacla.utils import getDetectorConfig, unique_ordered
+from py15sacla.utils import eqbinhistogram
